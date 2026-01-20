7652e118d55de2edc9dc0e8be05718f94641529d
coorwizzy <coreybramley15@gmail.com>
2026-01-20 02:28:18 +0000

    test
    test

---
+++ dictionary/Sprite2d.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class Sprite2d : Sprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		float Amount = 5;
+		if (Input.IsKeyPressed(Key.W)){
+			this.Position += new Vector2(0,-Amount);
+		}
+		if (Input.IsKeyPressed(Key.A)){
+			this.Position += new Vector2(-Amount,0);
+		}
+		if (Input.IsKeyPressed(Key.S)){
+			this.Position += new Vector2(0,Amount);
+		}
+		if (Input.IsKeyPressed(Key.D)){
+			this.Position += new Vector2(Amount,0);
+		}
+	}
+}
